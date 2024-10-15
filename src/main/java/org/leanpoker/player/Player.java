ab592dc7a5571d23591f0bf6a74d0796f2d22d00
Alastair Watts <alastair.watts@avrios.com>
2024-10-15 14:11:30 +0200

    parse to object

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,12 +1,15 @@
 package org.leanpoker.player;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     public static int betRequest(JsonNode request) {
+        GameState gameState = mapper.treeToValue(request, GameState.class);
         return 10;
     }
 
