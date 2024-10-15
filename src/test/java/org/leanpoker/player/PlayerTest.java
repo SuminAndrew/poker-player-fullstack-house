2ca22c31472dc5981395a89fc13e312ae303a3a7
Wentao Wang <wentao.wang@avrios.com>
2024-10-15 16:38:04 +0200

    Fix tests

--- src/test/java/org/leanpoker/player/PlayerTest.java
+++ src/test/java/org/leanpoker/player/PlayerTest.java
@@ -1,7 +1,6 @@
 package org.leanpoker.player;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.leanpoker.player.protocol.GameState;
